1db251886d5b4a79a12b204c6517cc14eddcaad5
Rod Li <rodney.lps@gmail.com>
2022-10-19 09:12:32 -0600

    Updates

---
+++ precache-manifest.9a96d4624adc0a10fd21ca18dfedd9cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72002434ab3c6c3d503372240789f60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27ca153a94b91fe3238b",
+    "url": "./static/css/main.b9b62618.chunk.css"
+  },
+  {
+    "revision": "7cd60976aec4e1f5c122",
+    "url": "./static/js/2.a52e6766.chunk.js"
+  },
+  {
+    "revision": "27ca153a94b91fe3238b",
+    "url": "./static/js/main.e20043cd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
