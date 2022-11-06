b3b9f6e79bb04e9de0e9adc4c7486355b903ef26
Rod Li <rodney.lps@gmail.com>
2022-11-06 07:21:01 -0700

    Updates

---
+++ precache-manifest.fec0f903fb8400822acfde2617927a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ca9a9618a774403744803052549b52",
+    "url": "/alscalcmobileRLtest/index.html"
+  },
+  {
+    "revision": "1847237833b0a609bbc8",
+    "url": "/alscalcmobileRLtest/static/css/2.ad109a12.chunk.css"
+  },
+  {
+    "revision": "04c2b1f6853a1babf62a",
+    "url": "/alscalcmobileRLtest/static/css/main.11ed17e6.chunk.css"
+  },
+  {
+    "revision": "1847237833b0a609bbc8",
+    "url": "/alscalcmobileRLtest/static/js/2.64b0c49d.chunk.js"
+  },
+  {
+    "revision": "04c2b1f6853a1babf62a",
+    "url": "/alscalcmobileRLtest/static/js/main.9de6b700.chunk.js"
+  },
+  {
+    "revision": "b26b860dd0b92ca6511a",
+    "url": "/alscalcmobileRLtest/static/js/runtime~main.71a3f89e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alscalcmobileRLtest/precache-manifest.62af28bb458ab2af1e878d57c12e4c67.js"
+  "/alscalcmobileRLtest/precache-manifest.fec0f903fb8400822acfde2617927a03.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alscalcmobileRLtest/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
