dd550768d1d604050c35775817705ff5f950c562
Rod Li <rodney.lps@gmail.com>
2022-11-09 20:56:54 -0700

    Updates

---
+++ precache-manifest.9465e1319ca2d606637ce8099e57baa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f01564477c61f7f94f84e089b6b523",
+    "url": "/alscalcmobileRLtest/index.html"
+  },
+  {
+    "revision": "be342d727819ee09010f",
+    "url": "/alscalcmobileRLtest/static/css/2.ad109a12.chunk.css"
+  },
+  {
+    "revision": "62914b8d2ff01ead074d",
+    "url": "/alscalcmobileRLtest/static/css/main.74e2a450.chunk.css"
+  },
+  {
+    "revision": "be342d727819ee09010f",
+    "url": "/alscalcmobileRLtest/static/js/2.13c278b1.chunk.js"
+  },
+  {
+    "revision": "62914b8d2ff01ead074d",
+    "url": "/alscalcmobileRLtest/static/js/main.56fde362.chunk.js"
+  },
+  {
+    "revision": "b26b860dd0b92ca6511a",
+    "url": "/alscalcmobileRLtest/static/js/runtime~main.71a3f89e.js"
+  }
+]);
