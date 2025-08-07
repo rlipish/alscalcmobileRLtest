4aed868ccefad55374e4b12a780c68e61dbcd185
Rod Li <rodney.lps@gmail.com>
2025-08-07 15:36:29 -0600

    swiper react 501

--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -1,66 +1,68 @@
-import React from "react";
+import React, { useRef, useState } from 'react';
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import SwipeableViews from "react-swipeable-views";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // core Swiper styles
+import "swiper/css/navigation"; // optional if you want nav buttons
+import "swiper/css/pagination"; // optional if you want pagination dots
 
 const styles = {
   tabs: {
     background: "#bfbfbf",
-    bottom: "0px"
+    bottom: "0px",
   },
-
   slide: {
     paddingTop: 0,
     minHeight: 0,
     color: "#000",
     display: "flex",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
 };
 
-class Panel extends React.Component {
-  state = {
-    index: 0
+const Panel = ({ findings, findings1, results, final, changed }) => {
+  const [index, setIndex] = useState(0);
+  let swiperInstance = null;
+
+  const handleChange = (event, value) => {
+    setIndex(value);
+    if (swiperInstance) swiperInstance.slideTo(value);
+    if (changed) changed();
   };
 
-  handleChange = (event, value) => {
-    this.setState({
-      index: value
-    });
-    this.props.changed();
+  const handleSlideChange = (swiper) => {
+    setIndex(swiper.activeIndex);
+    if (changed) changed();
   };
 
-  handleChangeIndex = index => {
-    this.setState({
-      index
-    });
-    this.props.changed();
+  return (
+    <div>
+      <Swiper
+        onSwiper={(swiper) => (swiperInstance = swiper)}
+        onSlideChange={handleSlideChange}
+        initialSlide={index}
+        spaceBetween={0}
+        slidesPerView={1}
+      >
+        <SwiperSlide style={styles.slide}>{findings}</SwiperSlide>
+        <SwiperSlide style={styles.slide}>{findings1}</SwiperSlide>
+        <SwiperSlide style={styles.slide}>{results}</SwiperSlide>
+        <SwiperSlide style={styles.slide}>{final}</SwiperSlide>
+      </Swiper>
 
-  };
-
-  render() {
-    const { index } = this.state;
-
-    return (
-      <div>
-        <SwipeableViews index={index} onChangeIndex={this.handleChangeIndex}>
-          {/* Is where we arrange order for findings and results at the bottom of the panel. */}
-          <div style={Object.assign({}, styles.slide, styles.slide3)}>{this.props.findings}</div>
-          <div style={Object.assign({}, styles.slide, styles.slide3)}>{this.props.findings1}</div>
-
-          <div style={Object.assign({}, styles.slide, styles.slide3)}>{this.props.results}</div>
-          <div style={Object.assign({}, styles.slide, styles.slide3)}>{this.props.final}</div>
-
-        </SwipeableViews>
-        <Tabs value={index} variant="fullWidth" onChange={this.handleChange} style={styles.tabs}>
-          <Tab label="Physical" />
-          <Tab label="Lab" />
-          <Tab label="Additional" />
-          <Tab label="Calculation" />
-        </Tabs>
-      </div>
-    );
-  }
-}
+      {/* <Tabs
+        value={index}
+        variant="fullWidth"
+        onChange={handleChange}
+        style={styles.tabs}
+      >
+        <Tab label="Physical" />
+        <Tab label="Lab" />
+        <Tab label="Additional" />
+        <Tab label="Calculation" />
+      </Tabs> */}
+    </div>
+  );
+};
 
 export default Panel;
